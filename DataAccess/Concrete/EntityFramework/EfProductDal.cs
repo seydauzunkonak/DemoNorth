ea6facfb6b106dccf59e1b386a6d5ef9814ff7a4
seydauzunkonak <52824376+seydauzunkonak@users.noreply.github.com>
2020-08-19 01:14:10 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+   public  class EfProductDal : IProductDal
+    {
+    }
+}
